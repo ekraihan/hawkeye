f542c06433cefb290b3d99fceb7316910b7a0aa0
ekraihan <ekraihan@iupui.edu>
2017-09-08 20:02:45 -0400

    refining the way I handle dates

--- class_alg.js
+++ class_alg.js
@@ -2,15 +2,36 @@ var todos = require("./fa17.json").todos
 var alasql = require("alasql")
 const {SingularValueDecomposition, Matrix} = require("ml-matrix")
 
-todos = alasql("SELECT DATE(due) as due, name, _class, time, importance FROM ? order by due", [todos])
-todos_untouched = alasql("SELECT DATE(due) as due, name, _class, time, importance FROM ? order by due", [todos])
+function rate_dates(date_array){
+    var diff_array = []
+
+    date_array.forEach(date => {
+        diff_array.push(date - new Date());
+    })
+    var sum_diffs = diff_array.reduce((total,num) => total+num);
+
+    var rating_array = [];
+    diff_array.forEach(diff => rating_array.push(sum_diffs/diff))
+
+    var rating_sum = rating_array.reduce((total,num) => total+num);
+
+    var stochastic_ratings = []
+    rating_array.forEach(rating => stochastic_ratings.push(rating/rating_sum))
+
+    return stochastic_ratings;
+}
+
+todos = alasql("SELECT DATE(due) as due, name, _class, time, importance FROM ?", [todos]);
+
+var date_ratings = rate_dates(alasql("SELECT due from ?", [todos]).map(object => object.due));
+todos_untouched = alasql("SELECT DATE(due) as due, name, _class, time, importance FROM ?", [todos])
 
 var sum_time = alasql("Select SUM(time) as time from ?", [todos]).map(object => object.time)
 var sum_importance = alasql("Select SUM(importance) as importance from ?", [todos]).map(object => object.importance)
 
 var final_results = {}
 todos.forEach((todo, index) => {
-    todos[index].urgency = (todos.length-1-index)/(((todos.length-1)*(todos.length)/2));
+    todos[index].urgency = date_ratings[index];
     todos[index].due = new Date(todo.due).toLocaleString()
     todos[index].time = todos[index].time/sum_time
     todos[index].importance = todos[index].importance/sum_importance
@@ -29,16 +50,16 @@ todos.forEach((todo) => {
 
 var data_matrix = new Matrix(data_matrix)
 var final_data = data_matrix.mmul(new Matrix([
-                            [.4],
                             [.1],
-                            [.5]
+                            [.1],
+                            [.8]
                         ])).to1DArray()
 
 final_data.forEach((data,index) => {
     todos[index].rating = data;
 })
 
-var orderd_work = alasql("Select name, _class, due, rating, time_u from ? order by rating desc", [todos]).map(object =>
+var orderd_work = alasql("Select name, _class, due, rating, time_u, importance from ? order by rating desc", [todos]).map(object =>
     "> "
     + object._class + " " + object.name
     + "\n"
@@ -47,6 +68,8 @@ var orderd_work = alasql("Select name, _class, due, rating, time_u from ? order 
     + "  Due: " + object.due
     + "\n"
     + "  Rating: " + object.rating.toFixed(2)
+    + "\n"
+    + "  Importance: " + object.importance.toFixed(1)
     + "\n")
 
 orderd_work.forEach(thing => console.log(thing))
